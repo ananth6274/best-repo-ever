c97fa4c87b5bea971a27aab7a7928a3b6ed3d4c5
Ananth Raja <ananth6274@yahoo.com>
2022-01-29 17:29:56 -0800

    my staged commit

--- force-app/main/default/aura/AccountSearch/AccountSearchController.js
+++ force-app/main/default/aura/AccountSearch/AccountSearchController.js
@@ -10,16 +10,3 @@
         helper.handleSearch( component, searchTerm );
     }
 })
-
-({
-    onInit: function( component, event, helper ) {
-        // proactively search on component initialization
-        var searchTerm = component.get( "v.searchTerm" );
-        helper.handleSearch( component, searchTerm );
-    },
-    onSearchTermChange: function( component, event, helper ) {
-        // search anytime the term changes in the input field
-        var searchTerm = component.get( "v.searchTerm" );
-        helper.handleSearch( component, searchTerm );
-    }
-})
